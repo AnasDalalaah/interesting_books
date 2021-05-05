6569210ca253d84836146738018583d74750e386
AnasDalalaah <>
2021-05-05 19:52:17 +0000

    pushing JS

---
+++ Java/app.js
@@ -0,0 +1,39 @@
+// Anas 
+/*
+
+Anas 
+Asmaa
+
+*/
+
+/*
+"Anas"
+
+4-4
+false
+true
+
+alert('hello');
+var F.N= Anas Dalalah;
+console.log(F.N);
+*/
+
+var favouriteMovie=prompt('what do you want to read fields of environment or economics?');
+
+
+
+if(favouriteBook=='Economist'){
+  document.write('<img src="https://prodimage.images-bn.com/pimages/9781633538009_p0_v3_s550x406.jpg"width="150px">');
+}
+else if(favouriteMovie === 'Environment'){
+  document.write('<img src="https://images1.penguinrandomhouse.com/cover/9780385546133" alt="Climate change" width="150px">');
+}else{
+  document.write('<h2> Please check the major entry next time</h2>');
+}
+
+
+var book=prompt('What is the most important book of Confucius?');
+
+if(book ==  Analects Confucius){
+  console.log('<h2>You are among the best</h2>')
+}
